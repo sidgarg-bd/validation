a962e2ed189c9fa254f216ac5af71a39bd30b3a3
Siddharth Garg <sidgarg.bd@gmail.com>
2020-09-11 15:43:57 +0530

    Change input file data

--- src/main/scala/com/scalaValidate/lengthValidate.scala
+++ src/main/scala/com/scalaValidate/lengthValidate.scala
@@ -14,7 +14,7 @@ object lengthValidate {
 
     val sc = new SparkContext(conf)
     val spark = SparkSession.builder.config(sc.getConf).getOrCreate()
-    val someDF = spark.read.format("csv").option("header", "true").option("delimiter","|").load("src/main/resources/test.csv")
+    val someDF = spark.read.format("csv").option("header", "true").load("src/main/resources/test.csv")
 
     def lengthCheck(df: DataFrame, colName: String, sze: Int, ignore: Boolean): Unit =
     {
@@ -37,8 +37,8 @@ object lengthValidate {
     }
 
 
-    lengthCheck(someDF, "agcy_id", 4, true)
-    lengthCheck(someDF, "agcy_id", 4, false)
+    lengthCheck(someDF, "word", 3, true)
+    lengthCheck(someDF, "word", 3, false)
     println("Bye from this App")
   }
 }
