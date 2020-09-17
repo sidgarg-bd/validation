d789a6f821316be8094c7485343ad7488fdbfe06
Siddharth Garg <sidgarg.bd@gmail.com>
2020-09-17 12:26:59 +0530

    Add inferschema for file reaading and add throwable exception Add output file

--- src/main/scala/com/scalaValidate/App.scala
+++ src/main/scala/com/scalaValidate/App.scala
@@ -49,6 +49,7 @@ object App {
 
       val someDF = spark.read.format("csv")
         .option("header", "true")
+        .option("inferSchema", "true")
         .load("src/main/resources/test.csv")
 
       val df1 = Check(someDF, List("class", "word"), "null", true)
@@ -57,23 +58,23 @@ object App {
       val df4 = Check(someDF, List("class", "word"), "empty", false)
       val df5 = Check(someDF, List("class", "word"), "empty", true)
 
-      val df11 = compCheck(someDF, List("class", "word"), "===", 3, true)
-      val df12 = compCheck(someDF, List("class", "word"), ">", 3, true)
-      val df13 = compCheck(someDF, List("class", "word"), "<", 3, true)
-      val df14 = compCheck(someDF, List("class", "word"), "<=", 3, true)
-      val df15 = compCheck(someDF, List("class", "word"), ">=", 3, true)
-      val df16 = compCheck(someDF, List("class", "word"), "===", 3, false)
-      val df17 = compCheck(someDF, List("class", "word"), ">", 3, false)
-      val df18 = compCheck(someDF, List("class", "word"), "<", 3, false)
-      val df19 = compCheck(someDF, List("class", "word"), "<=", 3, false)
-      val df20 = compCheck(someDF, List("class", "word"), ">=", 3, false)
-//      val df200 = compCheck(someDF, List("class", "word"), "====", 3, true)
+      val df11 = lengthCheck(someDF, "word", 3, true)
+      val df12 = lengthCheck(someDF, "word", 3, false)
 
-      val df21 = lengthCheck(someDF, "word", 3, true)
-      val df22 = lengthCheck(someDF, "word", 3, false)
+      val df21 = disCheck(someDF, true)
+      val df22 = disCheck(someDF, false)
 
-      val df31 = disCheck(someDF, true)
-      val df32 = disCheck(someDF, false)
+      val df31 = compCheck(someDF, List("class", "word"), "===", 3, true)
+      val df32 = compCheck(someDF, List("class", "word"), ">", 3, true)
+      val df33 = compCheck(someDF, List("class", "word"), "<", 3, true)
+      val df34 = compCheck(someDF, List("class", "word"), "<=", 3, true)
+      val df35 = compCheck(someDF, List("class", "word"), ">=", 3, true)
+      val df36 = compCheck(someDF, List("class", "word"), "===", 3, false)
+      val df37 = compCheck(someDF, List("class", "word"), ">", 3, false)
+      val df38 = compCheck(someDF, List("class", "word"), "<", 3, false)
+      val df39 = compCheck(someDF, List("class", "word"), "<=", 3, false)
+      val df40 = compCheck(someDF, List("class", "word"), ">=", 3, false)
+      val df400 = compCheck(someDF, List("class", "word"), "====", 3, true)
 
       println("Bye from this App")
     }
@@ -81,6 +82,7 @@ object App {
     catch{
       case x: AnalysisException => println(s"Analysis Exception: $x")
       case x: NullPointerException => println(s"Null Pointer Exception: $x")
+      case x:Throwable => println(s"Exception Thrown: $x")
       case unknown: Exception => println(s"Unknown Exception: $unknown")
     }
   }
