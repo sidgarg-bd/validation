3d318a81683750e18ca2382c85f3f2a5bd364e86
Siddharth Garg <sidgarg.bd@gmail.com>
2020-09-17 12:29:32 +0530

    Add exception for getting string type of data in list Corrected print statements

--- src/main/scala/com/scalaValidate/compValidate.scala
+++ src/main/scala/com/scalaValidate/compValidate.scala
@@ -2,10 +2,17 @@ package com.scalaValidate
 
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.{col,when}
+import org.apache.spark.sql.types.StringType
 
 object compValidate {
   def compCheck(df: DataFrame, ls: List[String], condition: String, compValue: Int, ignore: Boolean): DataFrame =
   {
+    for (colName <- ls){
+      df.schema(colName).dataType match {
+        case StringType => throw new Exception(s"$ls column's having data of String type but it must be of Integer type")
+        case _ => df
+      }
+    }
     var newDf = df.select(ls.map(col): _*)
     newDf = newDf.columns.foldLeft(newDf)((current, c) => current.withColumn(c, col(c).cast("int")))
     val list = Seq(">=","<=","<",">","===")
@@ -13,16 +20,18 @@ object compValidate {
     if (list.contains(condition)){
       if (ignore == true) {
         resultantDF = ignoreTrue(df: DataFrame, newDf: DataFrame, condition: String, compValue: Int)
+        println(s"Ignore True Comparison check case: Filter values for $condition $compValue in column's $ls")
         return resultantDF
       }
       else {
         resultantDF = ignoreFalse(df: DataFrame, newDf: DataFrame, condition: String, compValue: Int)
+        println(s"Ignore False Comparison check case: True for values for $condition $compValue in column's $ls")
         return resultantDF
       }
     }
     else{
       println(s"Your condition $condition is Invalid")
-      println(s"Pass valid Condition Out of: >=,<=,<,>,===")
+      println("Pass valid Condition Out of: >=,<=,<,>,===")
       System.exit(1)
       return resultantDF
     }
@@ -42,7 +51,6 @@ object compValidate {
     for(colName <- newFrame.columns){
       resDf = resDf.drop(newFrame.col(s"$colName"))
     }
-    println(s"Ignore True Comparison check case: Filter values for $cond")
     return resDf
   }
 
@@ -60,7 +68,6 @@ object compValidate {
     for(colName <- newFrame.columns){
       resDf = resDf.drop(newFrame.col(s"$colName"))
     }
-    println(s"Ignore False Comparison check case: True for values for $cond")
     return resDf
   }
 }
